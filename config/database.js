a87a32a42ec495e8a9f3d2fe4c54cb060254d2c9
Tomasz <harpoon2009@gmail.com>
2016-10-26 13:11:06 +0200

    Production database uri fixed.

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = function(env) {
     if (env === "test")
         url = "mongodb://admin-test:admin123@ds023704.mlab.com:23704/print-photo-test";
     else if (env === "production")
-        url = " mongodb://admin:admin123@ds051605.mlab.com:51605/print-photo";
+        url = "mongodb://admin:admin123@ds051605.mlab.com:51605/print-photo";
 
     return url;
 };
