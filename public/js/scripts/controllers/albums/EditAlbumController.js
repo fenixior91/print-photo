d01dac85bf2bf1709207f5a23c62d1d16a206f7f
Tomasz <harpoon2009@gmail.com>
2016-10-27 19:10:01 +0200

    Loading existing photos when user clicks the button.

--- public/js/scripts/controllers/albums/EditAlbumController.js
+++ public/js/scripts/controllers/albums/EditAlbumController.js
@@ -5,11 +5,13 @@
 "use strict";
 
 angular.module("app")
-    .controller("EditAlbumController", function($scope, $stateParams, Notification, AlbumService) {
+    .controller("EditAlbumController", function($scope, $stateParams, Notification, AlbumService, PhotoService) {
         var vm = this;
 
         vm.album = {};
+        vm.photos = [];
         vm.loading = true;
+        vm.existingPhotosEnable = false;
 
         AlbumService.getAlbum($stateParams.id)
             .then(function(result) {
@@ -20,4 +22,19 @@ angular.module("app")
                 Notification.error({message: error, positionX: 'center', delay: 1500});
                 vm.loading = false;
             });
+
+        vm.loadExistingPhotos = function() {
+            vm.existingPhotosEnable = !vm.existingPhotosEnable;
+
+            if (vm.photos.length === 0) {
+                PhotoService.getPhotos()
+                    .then(function(result) {
+                        vm.photos = result.data;
+                        console.log(vm.photos);
+                    })
+                    .catch(function(error) {
+                        Notification.error({message: error, positionX: 'center', delay: 1500});
+                    });
+            }
+        }
     });
